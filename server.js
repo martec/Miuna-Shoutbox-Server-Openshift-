69ff147f36f364c8c7588f20ca5bf779344d9972
martec <mr.martchero@gmail.com>
2016-10-02 10:42:33 +0900

    9.1

--- server.js
+++ server.js
@@ -301,10 +301,6 @@ function startall() {
 				}
 				data["newmsg"] = badwordreplace(data.newmsg);
 				db.updmsg(data, function(err, docs){
-					if (docs) {
-						docs["msg"] = xss(data["newmsg"]);
-						docs["edtusr"] = data["tk_eduser"];
-					}
 					nspm.emit('updmsg', docs);
 				});
 			}
